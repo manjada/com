78d7c5c59b30f1dcf2a4acd8ebac7ff09bc5e8bb
Ocky Anggoro <ockyanggoro@gmail.com>
2024-09-08 20:45:33 +0700

    add username field

--- model/user.go
+++ model/user.go
@@ -3,9 +3,10 @@ package model
 type User struct {
 	TransactionModel
 	Name     string `gorm:"type:varchar(255); not null"`
+	UserName string `gorm:"type:varchar(255); not null"`
 	Email    string `gorm:"index:idx_user_email; type:varchar(255); not null"`
 	Password string `gorm:"not null; type:varchar(255);"`
 	Status   string `gorm:"type:varchar(20); index:idx_user_status;"`
 	TryLogin *int64
-	Roles     []Role    `gorm:"many2many:user_role;" json:"roles" form:"roles"`
+	Roles    []Role `gorm:"many2many:user_role;" json:"roles" form:"roles"`
 }
