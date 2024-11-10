37a7b617603c35da191a5e244c2007612cf9c916
manjada <mjd.revo@gmail.com>
2024-11-10 12:52:09 +0700

    add audit log for each action

---
+++ model/audit_log.go
@@ -0,0 +1,11 @@
+package model
+
+type AuditLog struct {
+	TransactionModel
+	UserId    string `gorm:"type:varchar(255)"`
+	Username  string `gorm:"type:varchar(255)"`
+	IpAddress string `gorm:"type:varchar(255)"`
+	Module    string `gorm:"type:varchar(255)"`
+	Action    string `gorm:"type:varchar(255)"`
+	Detail    string `gorm:"type:text"`
+}
---
+++ svc/audit_log.go
@@ -0,0 +1,66 @@
+package svc
+
+import (
+	"encoding/json"
+	"github.com/manjada/com/db"
+	"github.com/manjada/com/db/repo"
+	"github.com/manjada/com/dto"
+	"github.com/manjada/com/model"
+)
+
+type AuditLogger interface {
+	LogCreate(userId, username, ipAddress, module string, detail interface{}) error
+	LogRead(userId, username, ipAddress, module string, detail interface{}) error
+	LogUpdate(userId, username, ipAddress, module string, detail interface{}) error
+	LogDelete(userId, username, ipAddress, module string, detail interface{}) error
+	LogApproved(userId, username, ipAddress, module string, detail interface{}) error
+	LogRejected(userId, username, ipAddress, module string, detail interface{}) error
+}
+
+func NewAuditLogService(DB db.DBConnector) AuditLogger {
+	return &AuditLogService{Db: repo.NewBaseRepo(DB)}
+}
+
+type AuditLogService struct {
+	Db repo.BaseRepoGorm
+}
+
+func (a *AuditLogService) LogCreate(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_CREATE, detail)
+}
+
+func (a *AuditLogService) LogRead(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_READ, detail)
+}
+
+func (a *AuditLogService) LogUpdate(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_UPDATE, detail)
+}
+
+func (a *AuditLogService) LogDelete(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_DELETE, detail)
+}
+
+func (a *AuditLogService) LogApproved(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_APPROVE, detail)
+}
+
+func (a *AuditLogService) LogRejected(userId, username, ipAddress, module string, detail interface{}) error {
+	return a.log(userId, username, ipAddress, module, dto.AUDIT_ACTION_REJECT, detail)
+}
+
+func (a *AuditLogService) log(userId, username, ipAddress, module, action string, data interface{}) error {
+	detail, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	auditLog := model.AuditLog{
+		UserId:    userId,
+		Username:  username,
+		IpAddress: ipAddress,
+		Module:    module,
+		Action:    action,
+		Detail:    string(detail),
+	}
+	return a.Db.Create(&auditLog).DbRepo.Error
+}
