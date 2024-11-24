bc0c84575e714e71368ac6f6c9f9fec8c265acfe
Ocky <ockyanggoro@gmail.com>
2024-11-24 13:48:01 +0700

    generate approval

--- repo/base.go
+++ repo/base.go
@@ -1,7 +1,9 @@
 package repo
 
 import (
+	"encoding/json"
 	"errors"
+	"github.com/manjada/com/dto"
 	"github.com/oklog/ulid"
 	"gorm.io/gorm"
 	"math/rand"
@@ -13,6 +15,68 @@ type TransactionModel struct {
 	CreatedAt time.Time      `gorm:"index"`
 	UpdatedAt time.Time      `gorm:"index"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (receive *TransactionModel) BeforeUpdate(tx *gorm.DB) error {
+	receive.UpdatedAt = time.Now()
+	return nil
+}
+
+func (receive *TransactionModel) AfterCreate(tx *gorm.DB) error {
+	tableName := tx.Statement.Table
+	var data map[string]interface{}
+	tx.Table(tableName).Where("id = ?", receive.Id).First(&data)
+	var dataBin []byte
+	dataBin, err := json.Marshal(&data)
+	if err != nil {
+		dto.ErrorDb(err)
+		return err
+	}
+	err = receive.buildApprovalTransaction(tx, tableName, data, dataBin)
+	if err != nil {
+		dto.ErrorDb(err)
+		return err
+	}
+	return nil
+}
+
+func (receive *TransactionModel) buildApprovalTransaction(tx *gorm.DB, tableName string, data map[string]interface{}, dataBin []byte) error {
+	var dataApproval []map[string]interface{}
+	err := tx.Table("approvals").
+		Select(`"approvals".id, "approval_details".approval_by, "approval_details".approval_name, "approval_details".client_id`).
+		Joins(`left join "approval_details".approval_id = "approvals".id`).
+		Where(`"approvals".module_menu_code = ? and "approval_details".client_id = ?`, tableName, data["client_id"].(string)).
+		Find(&dataApproval).Error
+	if err != nil {
+		return err
+	}
+
+	err = tx.Table("approval_transactions").Create(map[string]interface{}{
+		"approval_id":    dataApproval[0]["id"],
+		"client_id":      dataApproval[0]["client_id"],
+		"module_code":    dataApproval[0]["module_code"],
+		"status":         "Pending",
+		"reference_id":   receive.Id,
+		"total_approval": len(dataApproval),
+		"type":           dataApproval[0]["type"],
+		"data":           string(dataBin),
+	}).Error
+	if err != nil {
+		return err
+	}
+
+	for _, datas := range dataApproval {
+		err = tx.Table("approval_transaction_details").Create(map[string]interface{}{
+			"approval_transaction_id": datas["id"],
+			"approval_by":             data["approval_by"],
+			"approval_by_name":        data["approval_by_name"],
+		}).Error
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (receive *TransactionModel) BeforeCreate(tx *gorm.DB) error {
