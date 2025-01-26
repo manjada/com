cb3e4a29ac685f3d9e36362ee8361708dc955a6d
oqanggoro <ockyanggoro@gmail.com>
2025-01-26 20:00:29 +0700

    add filter struct and change filter into search

--- dto/page.go
+++ dto/page.go
@@ -3,11 +3,15 @@ package dto
 type Page struct {
 	Page   int    `json:"page"`
 	Size   int    `json:"size"`
-	Filter string `json:"filter"`
+	Search string `json:"search"`
 }
 
 type PageResponse struct {
 	Page  int `json:"page"`
 	Size  int `json:"size"`
 	Total int `json:"total"`
+}
+
+type Filter struct {
+	Filter map[string]interface{} `json:"filter"`
 }
