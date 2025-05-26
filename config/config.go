4f67387a6dbb574335435a0dd997b5e2411fc7c5
oqanggoro <ockyanggoro@gmail.com>
2025-05-26 21:04:27 +0700

    refactor: update database connection to use DSN format for Postgres adapter

--- config/config.go
+++ config/config.go
@@ -43,7 +43,6 @@ type Config struct {
 		Debug    bool   `mapstructure:"debug"`
 		Type     string `mapstructure:"type"`
 		Orm      string `mapstructure:"orm"`
-		Dsn      string `mapstructure:"dsn"`
 	} `mapstructure:"db_config"`
 
 	NoSqlConfig struct {
--- db_adapter/db.go
+++ db_adapter/db.go
@@ -1,6 +1,7 @@
 package db_adapter
 
 import (
+	"fmt"
 	"github.com/manjada/com/config"
 	"github.com/manjada/com/db_adapter/gorm"
 	_interface "github.com/manjada/com/db_adapter/interface"
@@ -13,7 +14,8 @@ func NewDbAdapter() _interface.DBAdapter {
 	switch cfg.DbConfig.Orm {
 	case "gorm":
 		if cfg.DbConfig.Type == "postgres" {
-			db, err = gorm.NewPostgresGormAdapter(cfg.DbConfig.Host)
+			dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s", cfg.DbConfig.Host, cfg.DbConfig.User, cfg.DbConfig.Pass, cfg.DbConfig.DbName, cfg.DbConfig.Port, cfg.DbConfig.Timezone)
+			db, err = gorm.NewPostgresGormAdapter(dsn)
 			if err != nil {
 				config.Panic(err)
 			}
